82b6e13b18b5f7ff88783071eb6e9adb6fd3178d
Searchstars <searchstars@163.com>
2024-05-03 15:06:09 +0800

    删除多余证书文件

--- RizServerConsole/certs/CreateNewCert_Windows.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Create a new cert need install OpenSSL-Win64. You can download it in "https://slproweb.com/products/Win32OpenSSL.html"
-openssl req -newkey rsa:2048 -nodes -keyout 0.key -x509 -days 365 -out 0.cer
-openssl pkcs12 -export -in 0.cer -inkey 0.key -out cert_output.pfx
-pause
